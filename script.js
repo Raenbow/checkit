b721bbe52503f4b945a1fae21b54e84cb76a6f43
delray <d.a.faskey@gmail.com>
2019-02-11 14:49:43 -0800

    creating movement rule functions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function initializeGame(){
 };
 
 function attachClickHandlers(){
-    playerSwitch();
+    playerPieceClickSwitch();
 };
 
 // ====================================================== gameObject
@@ -43,14 +43,21 @@ function spaceClickOff(){
     $('.space').off("click");
 }
 
-function playerSwitch(){
-    if(gameObj.playerTurn==='1'){
+function playerPieceClickSwitch(){
+    if (gameObj.playerTurn==='1'){
         p2ClickOff();
         p1ClickOn();
-    } else if(gameObj.playerTurn==='2'){
+    } else if (gameObj.playerTurn==='2'){
         p1ClickOff();
         p2ClickOn();
-    }
+    };
+};
+function playerTurnSwitch(){
+    if (gameObj.playerTurn==='1'){
+        gameObj.playerTurn = '2';
+    } else if (gameObj.playerTurn==='2'){
+        gameObj.playerTurn = '1';
+    };
 };
 
 // ====================================================== So Fresh So Clean
@@ -126,8 +133,8 @@ function movePieceTo(){
             // turn off space clicks
             spaceClickOff();
             // switch player turn
-            gameObj.playerTurn = '2';
-            playerSwitch();
+            playerTurnSwitch();
+            playerPieceClickSwitch();
 
         } else if(gameObj.playerTurn==='2'){
             // replace 0 with 1 in gameboard vairable where clicked
@@ -140,37 +147,77 @@ function movePieceTo(){
             // turn off space clicks
             spaceClickOff();
             // switch player turn
-            gameObj.playerTurn = '1';
-            playerSwitch();
+            playerTurnSwitch();
+            playerPieceClickSwitch();
         };
     };
 };
 
 function isItValid(){
     // Empty Space Check
-    if(gameObj.gameboard[gameObj.proposedMove.moveToRowIndex][gameObj.proposedMove.moveToSpaceIndex]==='0'){
-        if(gameObj.playerTurn==='1'){
+    if (gameObj.gameboard[gameObj.proposedMove.moveToRowIndex][gameObj.proposedMove.moveToSpaceIndex]==='0'){
+        ////// PLAYER #1
+        if (gameObj.playerTurn==='1'){
             // Single Forward Move
-            if(gameObj.proposedMove.moveToRowIndex===gameObj.selectedPiece.rowIndex+1){
+            if (move_p1_SingleForward()){
                 return true;
-            } else if(gameObj.gameboard[gameObj.proposedMove.moveToRowIndex-1][gameObj.proposedMove.moveToSpaceIndex-1]==='2' || gameObj.gameboard[gameObj.proposedMove.moveToRowIndex-1][gameObj.proposedMove.moveToSpaceIndex+1]==='2'){
+            } else if (move_p1_SingleJump()){
+                // if second jump is possible return false after running a function that keeps it from switching player turns
                 console.log('JUMPEH JUMPEH');
-                // run jump function
                 return true;
             }
-
-        } else if(gameObj.playerTurn==='2'){
+        ////// PLAYER #2
+        } else if (gameObj.playerTurn==='2'){
             // Single Forward Move
-            if(gameObj.proposedMove.moveToRowIndex===gameObj.selectedPiece.rowIndex-1){
+            if (move_p2_SingleForward()){
                 return true;
             };
         };
-    } else{
+    } else {
         console.log('you cant move that way!');
     };
 };
 
-function jump(){
+// ====================================================== Movement Rules
+
+// =========================== Player 1 Movement
+
+function move_p1_SingleForward(){
+    // down one row
+    if (gameObj.proposedMove.moveToRowIndex === gameObj.selectedPiece.rowIndex +1){
+        // left or right one space
+        if (move_SingleLeftRight()){
+            console.log('p1 single forward move');
+            return true;
+        }
+    };
+};
+function move_p1_SingleJump(){
+    if (gameObj.gameboard[gameObj.proposedMove.moveToRowIndex-1][gameObj.proposedMove.moveToSpaceIndex-1]==='2' 
+    || gameObj.gameboard[gameObj.proposedMove.moveToRowIndex-1][gameObj.proposedMove.moveToSpaceIndex+1]==='2'){
+        return true;
+    }
     //remove jumped piece
     //increase score
 }
+
+// =========================== Player 2 Movement
+
+function move_p2_SingleForward(){
+    // up one row
+    if (gameObj.proposedMove.moveToRowIndex === gameObj.selectedPiece.rowIndex -1){
+        // left or right one space
+        if (move_SingleLeftRight()){
+            console.log('- p2 single forward move');
+            return true;
+        }
+    }
+}
+
+// =========================== Universal Movement
+
+function move_SingleLeftRight(){
+    if (gameObj.proposedMove.moveToSpaceIndex === gameObj.selectedPiece.spaceIndex +1 || gameObj.proposedMove.moveToSpaceIndex === gameObj.selectedPiece.spaceIndex -1){
+        return true;
+    };
+};
